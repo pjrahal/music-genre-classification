9050b3fef5267f4e15ee85997a77b3edd2522d61
pjrahal <68881974+pjrahal@users.noreply.github.com>
2025-04-27 13:47:35 -0500

    Update genreCNN.py

--- genreCNN.py
+++ genreCNN.py
@@ -25,13 +25,13 @@ class GenreCNN(nn.Module):
             nn.Conv2d(3, 32, 3, padding=1),
             nn.BatchNorm2d(32),
             nn.GELU(),
-            nn.Dropout(0.1),
+            nn.Dropout(0.2),
             nn.MaxPool2d(2),
 
             nn.Conv2d(32, 64, 3, padding=1, groups=2),
             nn.BatchNorm2d(64),
             nn.GELU(),
-            nn.Dropout(0.1),
+            nn.Dropout(0.2),
             nn.MaxPool2d(2),
 
             nn.Conv2d(64, 128, 3, padding=1, groups=4),
